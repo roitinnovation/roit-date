3399b9291f96fb143ce43555b1754fa6f0623fff
Caio <caiow.wk@gmail.com>
2021-03-11 11:23:57 -0300

    test: ensure formatCompetence will return MM/yyyy format

--- test/date/DateFormat.spec.ts
+++ test/date/DateFormat.spec.ts
@@ -47,4 +47,14 @@ describe('DateFormat', () => {
          })
     })
 
+    describe('formatCompetence()', () => {
+        it('Should be able to parse a ISO format to MM/yyyy', () => {
+            const result = sut.formatCompetence('2021-01-12T03:00:00.000Z')
+
+            const expected = '01/2021'
+
+            expect(result).to.be.deep.equal(expected)
+        })
+    })
+
 })
