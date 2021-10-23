f0d825d8dc43cbfb9f6860a4747b0367212e8f7c
jeremiassilvaroit <jeremiassilvaroit@bitbucket.org>
2021-10-23 01:32:16 -0300

    add timezone options

--- src/domain/enums/Timezone.ts
+++ src/domain/enums/Timezone.ts
@@ -4,11 +4,13 @@ export enum Timezone {
     AMERICA_SAO_PAULO = 'America/Sao_Paulo'
 }
 
-export const listTimeZone = timezones.map(ti => {
-    return {
-        timezone: ti.tzCode,
-        label: ti.label,
-        name: ti.label,
-        utc: ti.utc
-    } 
-})
+export function listTimeZone() {
+    return timezones.map(ti => {
+        return {
+            timezone: ti.tzCode,
+            label: ti.label,
+            name: ti.label,
+            utc: ti.utc
+        } 
+    })
+}
